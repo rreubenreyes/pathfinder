36def8f08dcdfd29e3536ce76e743d7d36c21861
Reuben Reyes <reuben.a.reyes@gmail.com>
2019-05-25 12:22:40 -0700

    add: error handling framework, define keycodes

---
+++ src/util/error.js
@@ -0,0 +1,31 @@
+const _eq = A => B => A === B;
+
+const assertionLookup = {
+    IS: ['to have type of', (A, B) => typeof A === B],
+    OF: ['to be instance of', (A, B) => A instanceof B],
+    EQ: ['to equal', (A, B) => A === B],
+    NE: ['not to equal', (A, B) => A !== B],
+    GT: ['to be greater than', (A, B) => A > B],
+    GTE: ['to be greater than or equal to', (A, B) => A < B],
+    GT: ['to be greater than', (A, B) => A >= B],
+    LTE: ['to be less than or equal to', (A, B) => A <= B],
+    IN: ['to be an array containing', (A, B) => Array.isArray(A) && A.find(_eq(B)) ? true : false],
+    NIN: ['to be an array not containing', (A, B) => Array.isArray(A) && A.find(_eq(B)) ? true : false],
+    ON: ['to be an object with property', (A, B) => A.hasOwnProperty(B)],
+    NIN: ['to be an object without property', (A, B) => !A.hasOwnProperty(B)],
+}
+
+class AssertionError extends Error {
+    constructor(...args) {
+        super(...args);
+
+        this.name = 'AssertionError';
+        Error.captureStackTrace(this, AssertionError);
+    }
+}
+
+export default function assert(A, F, B) {
+    if (!assertionLookup[F][1](A, B)) {
+        throw new AssertionError(`Expected ${A} ${assertionLookup[F][0]} ${B}`);
+    }
+}
